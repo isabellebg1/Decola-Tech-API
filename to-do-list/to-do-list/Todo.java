eae209fb42a13a0953734cde5b7bd8beeae9c9ae
Isabelle <bellbebeca@hotmail.com>
2025-03-12 19:05:48 -0300

    Update Todo.java

--- to-do-list/to-do-list/Todo.java
+++ to-do-list/to-do-list/Todo.java
@@ -10,7 +10,7 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-    private String description;
-    private boolean completed;
+    private String titulo;
+    private String descricao;
+    private boolean completo;
 }
